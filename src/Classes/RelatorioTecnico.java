50a2ed4dd3f357c408ce8a2f165f8967b28605b7
Willian.git69 <willianclemente10@gmail.com>
2019-11-29 16:02:22 -0300

    classes

--- src/Classes/RelatorioTecnico.java
+++ src/Classes/RelatorioTecnico.java
@@ -6,7 +6,73 @@ package Classes;
  */
 public class RelatorioTecnico extends Trabalhos{
 
-    
+    private String autor;
+    private String instituicao;
+    private int ano;
+    private int NdePaginas;
+    private String palavraChave;
+    private String resumo;
+    private String abstract;
+
+    public RelatorioTecnico() {
+        super();
+    }
+
+    public String getAutor() {
+	    return this.autor;
+    }
+
+    public void setAutor(String autor) {
+	    this.autor = autor;
+    }
+
+    public String getInstituicao() {
+    	return this.instituicao;
+    }
+    public void setInstituicao(String instituicao) {
+    	this.instituicao = instituicao;
+    }
+
+
+    public int getAno() {
+    	return this.ano;
+    }
+    public void setAno(int ano) {
+    	this.ano = ano;
+    }
+
+
+    public int getNdePaginas() {
+    	return this.NdePaginas;
+    }
+    public void setNdePaginas(int NdePaginas) {
+    	this.NdePaginas = NdePaginas;
+    }
+
+
+    public String getPalavraChave() {
+    	return this.palavraChave;
+    }
+    public void setPalavraChave(String palavraChave) {
+    	this.palavraChave = palavraChave;
+    }
+
+
+    public String getResumo() {
+    	return this.resumo;
+    }
+    public void setResumo(String resumo) {
+    	this.resumo = resumo;
+    }
+
+
+    public String getAbstract() {
+    	return this.abstract;
+    }
+    public void setAbstract(String abstract) {
+    	this.abstract = abstract;
+    }
+
     
     
 }
--- src/Classes/Trabalhos.java
+++ src/Classes/Trabalhos.java
@@ -8,6 +8,24 @@ package Classes;
 public class Trabalhos {
     private String titulo;
     private String situacao;
+
+    public String getTitulo() {
+	    return this.titulo;
+    }
+    
+    public void setTitulo(String titulo) {
+	    this.titulo = titulo;    
+    }
+
+
+    public String getSituacao() {
+    	return this.situacao;
+    }
+    
+    public void setSituacao(String situacao) {
+    	this.situacao = situacao;
+    }
+
     
    
     
